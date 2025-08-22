ab2bd9f8eb4d9dcd9c3fbcc191fd7fa3c8f145e1
Øyvind A. Holm <sunny@sunbase.org>
2025-08-22 20:17:06 +0200

    selftest.c: Add OK_MEMCMP and OK_MEMCMP_L macros, 0:737
    The macros have the same arguments as OK_STRNCMP and OK_STRNCMP_L, but
    the buffers can contain null bytes.
    
    Commit ID: 5406385e-7f84-11f0-90d1-83850402c3ce

--- src/selftest.c
+++ src/selftest.c
@@ -113,6 +113,11 @@
  * Examples: OK_FALSE(user_exists(user), "User %s doesn't exist", user);
  *           OK_FALSE(result == 5, "Result is not 5");
  *
+ * OK_MEMCMP(a, b, len, desc, ...) - Compares `len` bytes of the buffers `a` 
+ * and `b` and succeeds if the buffers are identical. This macro differs from 
+ * OK_STRNCMP in that the buffers can contain null bytes.
+ * Example: OK_MEMCMP(buf1, buf2, 512, "Buffers are identical");
+ *
  * OK_NOTEQUAL(a, b, desc, ...) - Expects the values `a` and `b` to be 
  * different. The `!=` operator is used for the comparison.
  * Example: OK_NOTEQUAL(userid1, userid2, "The users have different IDs");
@@ -148,6 +153,7 @@
 #define OK_ERROR_L(linenum, msg, ...)  ok(1, (linenum), (msg), ##__VA_ARGS__)
 #define OK_FAILURE_L(func, linenum, desc, ...)  ok(!(func), (linenum), (desc), ##__VA_ARGS__)
 #define OK_FALSE_L(val, linenum, desc, ...)  ok(!!(val), (linenum), (desc), ##__VA_ARGS__)
+#define OK_MEMCMP_L(a, b, len, linenum, desc, ...)  ok(!!memcmp((a), (b), (len)), (linenum), (desc), ##__VA_ARGS__)
 #define OK_NOTEQUAL_L(a, b, linenum, desc, ...)  ok(!((a) != (b)), (linenum), (desc), ##__VA_ARGS__)
 #define OK_NOTNULL_L(p, linenum, desc, ...)  ok(!(p), (linenum), (desc), ##__VA_ARGS__)
 #define OK_NULL_L(p, linenum, desc, ...)  ok(!!(p), (linenum), (desc), ##__VA_ARGS__)
@@ -160,6 +166,7 @@
 #define OK_ERROR(msg, ...)  OK_ERROR_L(__LINE__, (msg), ##__VA_ARGS__)
 #define OK_FAILURE(func, desc, ...)  OK_FAILURE_L((func), __LINE__, (desc), ##__VA_ARGS__)
 #define OK_FALSE(val, desc, ...)  OK_FALSE_L((val), __LINE__, (desc), ##__VA_ARGS__)
+#define OK_MEMCMP(a, b, len, desc, ...)  OK_MEMCMP_L((a), (b), (len), __LINE__, (desc), ##__VA_ARGS__)
 #define OK_NOTEQUAL(a, b, desc, ...)  OK_NOTEQUAL_L((a), (b), __LINE__, (desc), ##__VA_ARGS__)
 #define OK_NOTNULL(p, desc, ...)  OK_NOTNULL_L((p), __LINE__, (desc), ##__VA_ARGS__)
 #define OK_NULL(p, desc, ...)  OK_NULL_L((p), __LINE__, (desc), ##__VA_ARGS__)
@@ -669,6 +676,9 @@ static void test_ok_macros(void)
 	/* OK_ERROR("OK_ERROR(...), can't be tested"); */
 	OK_FAILURE(1, "OK_FAILURE(1, ...)");
 	OK_FALSE(5 == 9, "OK_FALSE(5 == 9, ...)");
+	OK_MEMCMP("with\0null", "with\0null", strlen("with null") + 1,
+	          "OK_MEMCMP(\"with\\0null\", \"with\\0null\","
+	          " strlen(\"with null\") + 1, ...)");
 	OK_NOTEQUAL(19716, 1916, "OK_NOTEQUAL(%u, %u, ...)", 19716, 1916);
 	OK_NOTNULL(strstr("abcdef", "cde"),
 	           "OK_NOTNULL(strstr(\"abcdef\", \"cde\"), ...)");
@@ -3315,6 +3325,8 @@ int opt_selftest(char *main_execname, const struct Options *o)
 #undef OK_FAILURE_L
 #undef OK_FALSE
 #undef OK_FALSE_L
+#undef OK_MEMCMP
+#undef OK_MEMCMP_L
 #undef OK_NOTEQUAL
 #undef OK_NOTEQUAL_L
 #undef OK_NOTNULL
